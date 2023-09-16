69c2fec0f868e12b5534751a18a348613bf3dac4
kaabi <islamkaab13@gmail.com>
2023-09-17 01:31:37 +0600

    client communication completed and tested

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // used by client to send
+        // and receive data from socket
+        // object to read data from socket
+        final BufferedReader in;
+        // object to write data into socket
+        final PrintWriter out;
+        final Scanner sc = new Scanner(System.in);
+
+        try {
+            Socket clientSocket = new Socket("127.0.0.1", 5000);
+            out = new PrintWriter(clientSocket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            Thread sender = new Thread(new Runnable() {
+                String msg;
+                @Override
+                public void run() {
+                    while (true){
+                        msg = sc.nextLine();
+                        out.println(msg);
+                        out.flush();
+                    }
+
+                }
+            });
+            sender.start();
+
+            Thread receiver = new Thread(new Runnable() {
+                String msg;
+                @Override
+                public void run() {
+                    try {
+                        msg = in.readLine();
+                        while (msg!=null){
+                            System.out.println("Server : "+msg);
+                            msg = in.readLine();
+                        }
+                        System.out.println("Server disconnected");
+                        out.close();
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+            receiver.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
